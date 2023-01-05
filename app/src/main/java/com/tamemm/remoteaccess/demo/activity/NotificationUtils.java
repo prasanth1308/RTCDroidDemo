4ef4ca2263d83f1ea68e3c8a3e930da8179ed34b
Prasanth Ravi <prasanth1308@gmail.com>
2023-01-05 10:38:31 +0530

    Added sample widget ans renamed the app

--- app/src/main/java/com/tamemm/remoteaccess/demo/activity/NotificationUtils.java
+++ app/src/main/java/com/tamemm/remoteaccess/demo/activity/NotificationUtils.java
@@ -4,7 +4,9 @@ import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.annotation.NonNull;
@@ -23,6 +25,7 @@ public class NotificationUtils {
     public static Pair<Integer, Notification> getNotification(@NonNull Context context) {
         createNotificationChannel(context);
         Notification notification = createNotification(context);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
         NotificationManager notificationManager
                 = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(NOTIFICATION_ID, notification);
@@ -50,8 +53,17 @@ public class NotificationUtils {
         builder.setContentText(context.getString(R.string.recording));
         builder.setOngoing(true);
         builder.setCategory(Notification.CATEGORY_SERVICE);
-        builder.setPriority(Notification.PRIORITY_LOW);
+        builder.setPriority(NotificationManager.IMPORTANCE_HIGH);
         builder.setShowWhen(true);
+
+        Intent notificationIntent = new Intent(context, CallActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent intent = PendingIntent.getActivity(context, 0,
+                notificationIntent, 0);
+
+        builder.setContentIntent(intent);
+
         return builder.build();
     }
 
